aadb1d945ffc641fbc3faf1a4cbf268f82bada6a
PETER BRAIMOH <peterbraimoh98@gmail.com>
2022-09-18 15:39:37 +0100

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=monolithproject3
+export POSTGRES_PASSWORD=Racheal12345
+export POSTGRES_HOST=monolithproject3.cfjjsrf5rly9.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=monolithproject3
+export AWS_BUCKET=monolith-proj3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
